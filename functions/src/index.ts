8b663fe0146c1fed2607ef5c76ad3b4194fb79ab
Arvindan Baskaradoss <arvindnitroxp@gmail.com>
2025-06-16 19:32:46 +0100

    Fix deployment issues

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,17 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from "firebase-functions";
+import express, {Request, Response} from "express";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+export const helloWorld = functions.https.onRequest(
+  (req: Request, res: Response) => {
+    res.send("Hello from AutoAPI!");
+  }
+);
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("AutoAPI backend");
+});
+
+export const appFunc = functions.https.onRequest(app);
